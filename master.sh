18373e4ecea69841a38b3fbf000ddcc460ffb38c
JanStreffing <jan.streffing@awi.de>
2019-11-25 15:08:17 +0100

    add NAO to master postprocessing

--- master.sh
+++ master.sh
@@ -22,14 +22,17 @@ end=40
 
 for e in {11,16}
 do
-	for var in z500 U MSL T2M SD SF
+	for var in z500 U MSL T2M SD SF synact nao
 	do
 		./mean_for_scripts.sh $e $start $end $res $var $dir
 		./split_to_seasons.sh $e $start $end $res $var $dir
-		if [ "$var" == "z500" ]
+		if [ "$var" == "synact" ]
 		then
 			./synact_PAMIP.job $e $start $end $res $var $dir
   			./post_data_oifs_synact_stddev.job $e $start $end $res $var $dir
+		elif [ "$var" == "nao" ]
+		then
+	                ./post_data_oifs_nao.job $e $start $end $res $var $dir
 		fi
 	done
 done
