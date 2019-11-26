4989305c02f8c51d14b540ee10951fc2ccb2838e
Jan Streffing <jan.streffing@awi.de>
2019-11-26 10:54:49 +0100

    improving nao script and master.sh

--- master.sh
+++ master.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
 start=1
-end=40
-res=T1279
-dir='/p/largedata/hhb19/jstreffi/runtime/oifsamip/'
+end=100
+res=T159
+dir='/mnt/lustre01/work/ba1035/a270092/runtime/oifsamip'
 
 for e in {11,16}
 do
 	for var in z500 U MSL T2M SD SF
 	do
-		./prep_for_scripts.sh $e $start $end $res $var $dir
 		if [ "$var" == "z500" ]
 		then
 			./z500_cat.sh $e $start $end $res $var $dir
+		else
+			./prep_for_scripts.sh $e $start $end $res $var $dir
 		fi
 	done
 done
 
 start=1
-end=40
+end=100
 
 for e in {11,16}
 do
-	for var in z500 U MSL T2M SD SF synact nao
+	for var in z500 U MSL T2M SD SF synact NAO
 	do
-		./mean_for_scripts.sh $e $start $end $res $var $dir
-		./split_to_seasons.sh $e $start $end $res $var $dir
 		if [ "$var" == "synact" ]
 		then
 			./synact_PAMIP.job $e $start $end $res $var $dir
   			./post_data_oifs_synact_stddev.job $e $start $end $res $var $dir
-		elif [ "$var" == "nao" ]
+		elif [ "$var" == "NAO" ]
 		then
 	                ./post_data_oifs_nao.job $e $start $end $res $var $dir
+		else
+			./mean_for_scripts.sh $e $start $end $res $var $dir
+			./split_to_seasons.sh $e $start $end $res $var $dir
 		fi
+		
 	done
 done
