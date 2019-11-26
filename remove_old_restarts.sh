21ebe509ae7ab84e89eee52406185ea7cef2a854
JanStreffing <jan.streffing@awi.de>
2019-11-26 17:34:06 +0100

    adding post and input folder to cleanup

--- remove_old_restarts.sh
+++ remove_old_restarts.sh
@@ -1,10 +1,11 @@
 #! /bin/ksh -l
 
-e=16
+#e=16
 
-#for e in {11}
-#do
-  for i in {1..20}
+for e in 11 16
+do
+  cd /p/largedata/hhb19/jstreffi/runtime/oifsamip/T1279/
+  for i in {2..40}
   do
     echo "        ====================================================="
     echo "        Removing all but the last restart for $(printf "%03g" i)"
@@ -17,6 +18,7 @@ e=16
     rm -rf Experiment_${e}/E$(printf "%03g" i)/restart/oifs/00005
     rm -rf Experiment_${e}/E$(printf "%03g" i)/restart/oifs/00006
     rm -rf Experiment_${e}/E$(printf "%03g" i)/restart/oifs/00007
-
+    rm -rf Experiment_${e}/E$(printf "%03g" i)/post*
+    rm -rf Experiment_${e}/E$(printf "%03g" i)/input*
   done
-#done
+done
