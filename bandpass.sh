c2f049408ef31a46e55eaa72500c7ed232eaec0b
JanStreffing <jan.streffing@awi.de>
2020-03-20 19:32:05 +0100

    doing bandpass in ncl with lanczos filter

--- bandpass.sh
+++ bandpass.sh
@@ -22,26 +22,41 @@ do
 	pwd
 	if [ $res == 'T159' ]
 	then
-		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -bandpass,60,242 -sellonlatbox,-180,180,0,90 -seltimestep,976/1335 ../00001/6h_VO_00001.nc VO_bpf &
-		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -bandpass,60,242 -sellonlatbox,-180,180,0,90 -seltimestep,976/1335 ../00001/6h_V_00001.nc V_bpf &
-		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -bandpass,60,242 -sellonlatbox,-180,180,0,90 -seltimestep,976/1335 ../00001/6h_U_00001.nc U_bpf &
+		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -sellonlatbox,-180,180,0,90 -seltimestep,944/1367 ../00001/6h_VO_00001.nc VO_bpf_prep &
+		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -sellonlatbox,-180,180,0,90 -seltimestep,944/1367 ../00001/6h_V_00001.nc V_bpf_prep &
+		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -sellonlatbox,-180,180,0,90 -seltimestep,944/1367 ../00001/6h_U_00001.nc U_bpf_prep &
 		wait
-		cdo timstd VO_bpf VO_std &
-		cdo timstd V_bpf V_std &
-		cdo timstd U_bpf U_std &
+
+
 	else
 		for RUN_NUMBER_oifs in {2..7}
 		do
 			printf "      Leg number ${l}\n"
+			cdo -s cat ../$(printf "%05g" l)/6h_VO_$(printf "%05g" l).nc VO_cat.nc &
 			cdo -s cat ../$(printf "%05g" l)/6h_V_$(printf "%05g" l).nc V_cat.nc &
 			cdo -s cat ../$(printf "%05g" l)/6h_U_$(printf "%05g" l).nc U_cat.nc &
 			wait
 		done
-		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -bandpass,182,730 -sellonlatbox,-180,180,0,90 -seltimestep,732/1091 VO_cat.nc VO_bpf &
-		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -bandpass,182,730 -sellonlatbox,-180,180,0,90 -seltimestep,732/1091 V_cat.nc V_bpf &
-		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -bandpass,182,730 -sellonlatbox,-180,180,0,90 -seltimestep,732/1091 U_cat.nc U_bpf &
+		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -sellonlatbox,-180,180,0,90 -seltimestep,700/1123 VO_cat.nc VO_bpf_prep &
+		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -sellonlatbox,-180,180,0,90 -seltimestep,700/1123 V_cat.nc V_bpf_prep &
+		/p/project/chhb19/jstreffi/software/miniconda3/envs/pyn_env_py2/bin/cdo -P 8 -sellonlatbox,-180,180,0,90 -seltimestep,700/1123 U_cat.nc U_bpf_prep &
 		wait
 	fi
+
+	export var=VO
+	ncl /p/project/chhb19/jstreffi/postprocessing/ncl-plot-scripts/bandpass.ncl &
+	export var=V
+	ncl /p/project/chhb19/jstreffi/postprocessing/ncl-plot-scripts/bandpass.ncl &
+	export var=U
+	ncl /p/project/chhb19/jstreffi/postprocessing/ncl-plot-scripts/bandpass.ncl &
+	wait
+	cdo -seltimestep,32/392 VO_bpf.nc VO_bpf
+	cdo -seltimestep,32/392 V_bpf.nc V_bpf
+	cdo -seltimestep,32/392 U_bpf.nc U_bpf
+
+	cdo timstd VO_bpf VO_std &
+	cdo timstd V_bpf V_std &
+	cdo timstd U_bpf U_std &
 
 	cdo -timmean -mul U_bpf VO_bpf COV_U_VO.nc
 	cdo -timmean -mul V_bpf VO_bpf COV_V_VO.nc
@@ -52,7 +67,7 @@ do
 	cdo -sqrt -add COV_U_VO2.nc COV_V_VO2.nc Magnitude.nc
 
 	file_string+="$dir/$res/Experiment_${e}/E$(printf "%03g" i)/outdata/oifs/bandpass/EV.nc "
-	#rm M.nc N.nc U2-V2 U2_bpf V2_bpf U_bpf V_bpf
+	rm M.nc N.nc U2-V2 U2_bpf* V2_bpf* U_bpf* V_bpf* COV*
 done
 
 mkdir $dir/$res/Experiment_${e}/ensemble_mean
